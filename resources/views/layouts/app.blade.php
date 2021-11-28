b684768414ed618dc957dc5720df00c5c1121893
Qaiser Shakoor <link2qaiser@gmail.com>
2021-11-28 23:52:15 +0500

    Upload the input masking

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,15 @@
                   <li class="nav-item">
                      <a class="nav-link" href="{{url('dashboard/products')}}">Products</a>
                   </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Demos
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="{{url('dashboard/input-masking')}}">Input Masking</a>
+                      
+                    </div>
+                  </li>
                </ul>
             </div>
          </nav>
